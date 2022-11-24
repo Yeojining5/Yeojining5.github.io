33f7766f388a6c596d24b2933e5f62a6b94ace27
yeojining5 <yeojining5@gmail.com>
2022-11-25 04:48:48 +0900

    minimal blog

---
+++ site-meta-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // You can overwrite values here that are used for the SEO component
+  // You can also add new values here to query them like usual
+  // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
+  siteTitle: `Yeojin's Log`,
+  siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+  siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+  siteUrl: `https://Yeojining5.github.io/`,
+  siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+  siteImage: `/banner.jpg`,
+  author: `@Yeojining5`,
+
+  social: {
+    github: `yeojining5@gmail.com`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``,
+  },
+  icon: `content/assets/logo_haong.png`, // Add your favicon
+  keywords: [
+    `blog`,
+    `gatsby`,
+    `application`,
+    `react`,
+    `FontEnd`,
+    `Developer`,
+  ],
+  comment: {
+    utterances: 'Yeojin/my-blog-old', // Your repository for archive comment
+  },
+}
