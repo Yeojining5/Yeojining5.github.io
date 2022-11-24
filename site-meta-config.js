dba114472554dd093a3a366757ddaa3601294daa
yeojining5 <yeojining5@gmail.com>
2022-11-24 16:00:54 +0900

    Gatsby

---
+++ site-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `Yeojin's Log`,
+  subtitle: `Yeojin's Log`,
+  description: `Yeojin's Log`,
+  author: {
+    name: `@Yeojining5`,
+    summary: `live in SeongNam, Korea`,
+  },
+  introduction: `안녕하세요.`,
+  siteUrl: `https://Yeojining5.github.io/`, // Your blog site url
+  social: {
+    github: `yeojining5@gmail.com`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``,
+  },
+  icon: `content/assets/logo_haong.png`, // Add your favicon
+  keywords: [
+    `blog`,
+    `gatsby`,
+    `application`,
+    `react`,
+    `FontEnd`,
+    `Developer`,
+    `Yeojin's Log`,
+  ],
+  comment: {
+    utterances: 'Yeojin/my-blog-old', // Your repository for archive comment
+  },
+}
